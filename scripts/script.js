063f085c6be493c6e5c6b08efa5fbbbe179621b1
EduEnd <144707466+EduEnd@users.noreply.github.com>
2024-03-13 10:25:22 -0300

    Integrando sidebar com menu, adicionando java script usando a tag script
    foram dividido a parte css de  como de mapa e menu para facilitar a visualização e organização

---
+++ scripts/script.js
@@ -0,0 +1,4 @@
+//Script para o botao do sidebar abrir ao clique
+document.getElementById('abri_btn').addEventListener('click',function(){
+    document.getElementById('sidebar').classList.toggle('abri-sidebar');    
+   });
