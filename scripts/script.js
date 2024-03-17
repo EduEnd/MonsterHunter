b3e76a153c3abdcc21adbed88befefa191c2f214
EduEnd <144707466+EduEnd@users.noreply.github.com>
2024-03-17 15:52:41 -0300

    excluindo o video local e a pasta videos e adição da barra de pesquisa para as paginas

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,15 @@
 //Script para o botao do sidebar abrir ao clique
 document.getElementById('abri_btn').addEventListener('click',function(){
     document.getElementById('sidebar').classList.toggle('abri-sidebar');    
-   });
+    });
+
+let boxBuscar= document.querySelector('.buscar-box');
+let lupa = document.querySelector('.lupa-buscar');
+let btnfechar = document.querySelector('.btn-fechar');
+
+lupa.addEventListener('click', ()=>{
+    boxBuscar.classList.add('ativar')
+})
+btnfechar.addEventListener('click', ()=>{
+    boxBuscar.classList.remove('ativar')
+})
